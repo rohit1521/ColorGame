cb1991f9bd86407c8b05545426514280c3ab2f60
rohit1521 <gzsamp@gmail.com>
2019-06-16 15:52:47 +0530

    Initial

--- color.js
+++ color.js
@@ -1,15 +1,11 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 78, 0)",
-    "rgb(255, 0, 84)",
-    "rgb(215, 0, 140)",
-    "rgb(100, 100, 100)",
-    "rgb(25, 24, 80)"
-]
-
-winningColor = colors[3];
-
+var colors = []
+var h1 = document.querySelector("h1");
+for(var i = 0;i < 6;i++){
+colors[i] = "rgb("+Math.floor((Math.random() * 255) + 1)+ ", "+Math.floor((Math.random() * 255) + 1)+", "+Math.floor((Math.random() * 255) + 1)+")";
+}
+var winningColor = colors[Math.floor((Math.random() * 6) + 1)];
 var colorShow = document.querySelector("#colorShow");
+var barText = document.querySelector("#topBar");
 
 colorShow.textContent = winningColor;
 var squreColors = document.querySelectorAll(".square");
@@ -17,9 +13,17 @@ for(var i =0 ;i < colors.length ; i++){
     squreColors[i].style.backgroundColor = colors[i];
     squreColors[i].addEventListener("click",function(){
         if(this.style.backgroundColor === winningColor){
-            alert("Correct!");
+            changeColor(winningColor);
+            topBar.textContent = "Correct!";
+            h1.style.backgroundColor = winningColor;
         }else{
-            alert(this.style.backgroundColor);
+            this.style.backgroundColor = "#232323";
         }
     })
+}
+
+function changeColor(color){
+    for(var i=0;i<squreColors.length;i++){
+        squreColors[i].style.backgroundColor = color;
+    }
 }
